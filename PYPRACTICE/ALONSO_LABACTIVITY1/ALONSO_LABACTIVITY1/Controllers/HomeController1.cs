a29584c195a14f42e22b89effefe6aba79d63e4e
unknown <lemuelalonso691@gmail.com>
2026-02-14 14:11:41 +0800

    Updated Project at home

---
+++ PYPRACTICE/ALONSO_LABACTIVITY1/ALONSO_LABACTIVITY1/Controllers/HomeController1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ALONSO_LABACTIVITY1.Models;
+
+namespace ALONSO_LABACTIVITY1.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            Student pupil = new Student()
+            {
+                Id = 1,
+                Name = "Christopher Columbus",
+                Course = "BSIT"
+                
+
+            };
+            return View(pupil);
+        }
+    }
+}
---
+++ PYPRACTICE/ALONSO_LABACTIVITY1/ALONSO_LABACTIVITY1/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace ALONSO_LABACTIVITY1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+
+		// Classic method syntax
+		public string FullInfo()
+		{
+			return Id + " - " + Name + " - " + Course;
+		}
+
+		public string Greeting()
+		{
+			return "Hello, " + Name + "!";
+		}
+
+		public string YearLevel()
+		{
+			return "3rd Year";
+		}
+
+
+	}
+}
